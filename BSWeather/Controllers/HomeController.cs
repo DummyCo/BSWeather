36341d862094937b90c981c90360a387ffa2b3dd
DummyCo <ashurillia@gmail.com>
2016-07-07 00:35:51 +0300

    Basic JSON parsing and Controller-View interactions add

--- BSWeather/Controllers/HomeController.cs
+++ BSWeather/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
+using BSWeather.Models;
+using Newtonsoft.Json;
 
 namespace BSWeather.Controllers
 {
@@ -10,6 +16,13 @@ namespace BSWeather.Controllers
     {
         public ActionResult Index()
         {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Kiev&APPID=" + WebConfigurationManager.AppSettings["OpenWeatherMapAPIKEY"];
+            var webClient = new WebClient();
+            var resultString = webClient.DownloadString(url);
+
+            var weather = JsonConvert.DeserializeObject<WeatherBase.RootObject>(resultString);
+            ViewData["Weather"] = weather;
+
             return View();
         }
 
