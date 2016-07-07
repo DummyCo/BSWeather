6b79672304bde07c8543faf15f5104d738e0c56f
DummyCo <ashurillia@gmail.com>
2016-07-07 22:30:00 +0300

    Centred form add

---
+++ BSWeather/Models/CitySearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSWeather.Models
+{
+    public class CitySearch
+    {
+        public string CityName { get; set; }
+    }
+}
