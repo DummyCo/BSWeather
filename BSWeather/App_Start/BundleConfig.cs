f8ac704fdcdf0f8d4301358a4fd434435180acab
DummyCo <ashurillia@gmail.com>
2016-07-08 13:29:42 +0300

    Bootstrap unite theme add

--- BSWeather/App_Start/BundleConfig.cs
+++ BSWeather/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace BSWeather
                       "~/Scripts/validator.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-united.css",
                       "~/Content/Site.css"));
         }
     }
