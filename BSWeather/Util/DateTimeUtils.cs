3001049c42c54c0ad4830cadd4d66ce0cbbfd155
DummyCo <ashurillia@gmail.com>
2016-07-08 04:07:26 +0300

    DateTimeUtils add

--- BSWeather/Util/DateTimeUtils.cs
+++ BSWeather/Util/DateTimeUtils.cs
@@ -6,10 +6,7 @@ namespace BSWeather.Util
     {
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
-            // Unix timestamp is seconds past epoch
-            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
-            return dtDateTime;
+            return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(unixTimeStamp).ToLocalTime();
         }
     }
 }
