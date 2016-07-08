d8e3df7797db5b9f2406115358ec88f9c589bbdf
DummyCo <ashurillia@gmail.com>
2016-07-08 04:07:49 +0300

    Index page style extension

---
+++ BSWeather/Extensions/OpenWeatherBaseExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BSWeather.Models;
+using BSWeather.Util;
+
+namespace BSWeather.Extensions
+{
+    public static class OpenWeatherBaseExtensions
+    {
+        public static DateTime GetDateTime(this OpenWeatherBase.List weatherList)
+        {
+            return DateTimeUtils.UnixTimeStampToDateTime(weatherList.Dt);
+        }
+    }
+}
