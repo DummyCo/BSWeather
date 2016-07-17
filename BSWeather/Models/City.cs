eb7e78e9b3d004d31f7e1107d62bd2a31e9fad41
DummyCo <ashurillia@gmail.com>
2016-07-17 21:59:52 +0300

    Tiny model initialization polish

--- BSWeather/Models/City.cs
+++ BSWeather/Models/City.cs
@@ -4,17 +4,12 @@ namespace BSWeather.Models
 {
     public class City
     {
-        public City()
-        {
-            Users = new HashSet<User>();
-        }
-
         public int Id { get; set; }
 
         public string Name { get; set; }
         
         public int ExternalIdentifier { get; set; }
 
-        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<User> Users { get; set; } = new HashSet<User>();
     }
 }
--- BSWeather/Models/User.cs
+++ BSWeather/Models/User.cs
@@ -5,14 +5,8 @@ namespace BSWeather.Models
 {
     public class User : IdentityUser
     {
-        public User()
-        {
-            Cities = new HashSet<City>();
-            SearchHistoryRecords = new HashSet<SearchHistoryRecord>();
-        }
+        public virtual ICollection<City> Cities { get; set; } = new HashSet<City>();
 
-        public virtual ICollection<City> Cities { get; set; }
-
-        public virtual ICollection<SearchHistoryRecord> SearchHistoryRecords { get; set; }
+        public virtual ICollection<SearchHistoryRecord> SearchHistoryRecords { get; set; } = new HashSet<SearchHistoryRecord>();
     }
 }
