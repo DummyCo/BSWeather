5d170a9562a65ce6c33903dc76f1811bd5fb6773
DummyCo <ashurillia@gmail.com>
2016-07-17 23:17:44 +0300

    AnonymousOnly AuthorizeAttribute add

---
+++ BSWeather/Infrastructure/Attributes/AuthorizeAttributes/AnonymousOnly.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BSWeather.Infrastructure.Attributes.AuthorizeAttributes
+{
+    public class AnonymousOnly : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+            }
+        }
+    }
+}
